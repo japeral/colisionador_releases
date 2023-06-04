4a7f246590c373f7d5208ae39bd3bb505faea555
Jose Peral <japeralsoler@gmail.com>
2023-06-04 17:32:28 +0200

    select arch in start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,10 @@
-./colisionador -list r160list_sorted.csv -puzzle 66 -rand_pk -threads 24
+#!/bin/bash
+
+# Check for Architecture
+if [[ $(uname -m) == "x86_64" ]]; then
+    ./colisionador_x86_64 -list r160list_sorted.csv -puzzle 66 -rand_pk -threads 24
+else    
+    ./colisionador_armv7l -list r160list_sorted.csv -puzzle 66 -rand_pk -threads 24
+fi
+
+cat found.txt

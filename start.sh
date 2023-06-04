e9d96fca247410e59b84ff1d707c949f88ccbbfc
Jose Peral <japeralsoler@gmail.com>
2023-06-04 17:54:50 +0200

    modified threads to 8 for rpi4

--- start.sh
+++ start.sh
@@ -2,9 +2,9 @@
 
 # Check for Architecture
 if [[ $(uname -m) == "x86_64" ]]; then
-    ./colisionador_x86_64 -list r160list_sorted.csv -puzzle 66 -rand_pk -threads 24
+    ./colisionador_x86_64 -list r160list_sorted.csv -puzzle 66 -rand_pk -threads 64
 else    
-    ./colisionador_armv7l -list r160list_sorted.csv -puzzle 66 -rand_pk -threads 24
+    ./colisionador_armv7l -list r160list_sorted.csv -puzzle 66 -rand_pk -threads 8
 fi
 
 cat found.txt

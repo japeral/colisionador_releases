a8e9322abea58c86486ccbfc7fc65fe1f2b0e61f
Jose <japeralsoler@gmail.com>
2023-06-15 20:54:06 +0200

    automatic release

--- test.sh
+++ test.sh
@@ -2,9 +2,9 @@
 
 # Check for Architecture
 if [[ $(uname -m) == "x86_64" ]]; then
-    ./colisionador_x86_64 -list puzzle_r160list_sorted.csv -single -start_pk_bin 0000000000000000000000000000000000000000000000000000000000000001
+    ./colisionador_x86_64 -list puzzle_r160list_sorted.csv -puzzle 0 -threads 1 -start_pk_bin 0000000000000000000000000000000000000000000000000000000000000001
 else    
-    ./colisionador_armv7l -list puzzle_r160list_sorted.csv -single -start_pk_bin 0000000000000000000000000000000000000000000000000000000000000001
+    ./colisionador_armv7l -list puzzle_r160list_sorted.csv -puzzle 0 -threads 1 -start_pk_bin 0000000000000000000000000000000000000000000000000000000000000001
 fi
 
 cat found.txt

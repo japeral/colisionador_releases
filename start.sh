85d64b0df9d739c6b69b48453821a3161fed83c7
Jose <japeralsoler@gmail.com>
2023-06-25 05:07:02 +0200

    added xeon phi detection in .sh files

--- start.sh
+++ start.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
 # Check for Architecture
-if [[ $(uname -m) == "x86_64" ]]; then
+arch=$(uname -m)
+if   [[ "$arch" == "x86_64" ]]; then
     ./colisionador_x86_64 -puzzle 66
-else    
+elif [[ "$arch" == "aarch64" ]]; then  
     ./colisionador_armv7l -puzzle 66
+elif [[ "$arch" == "k1om" ]]; then  
+    ./colisionador_k1om-mpss.mic -puzzle 66
+else
+    echo "Not compatible architecture"
 fi
 
 cat found.txt
--- test.sh
+++ test.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
 # Check for Architecture
-if [[ $(uname -m) == "x86_64" ]]; then
+arch=$(uname -m)
+if   [[ "$arch" == "x86_64" ]]; then
     ./colisionador_x86_64 -list puzzle_r160list_sorted.csv -puzzle 0 -threads 1 -start_pk_bin 0000000000000000000000000000000000000000000000000000000000000001
-else    
+elif [[ "$arch" == "aarch64" ]]; then  
     ./colisionador_armv7l -list puzzle_r160list_sorted.csv -puzzle 0 -threads 1 -start_pk_bin 0000000000000000000000000000000000000000000000000000000000000001
+elif [[ "$arch" == "k1om" ]]; then  
+    ./colisionador_k1om-mpss.mic -list puzzle_r160list_sorted.csv -puzzle 0 -threads 1 -start_pk_bin 0000000000000000000000000000000000000000000000000000000000000001
+else
+    echo "Not compatible architecture"
 fi
 
 cat found.txt
